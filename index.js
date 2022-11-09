0cfc1ac3c8d3f53b6eeb28cc7752e02596c5152d
mikirtumov <mikirtumov@gmail.com>
2022-11-09 00:11:13 -0800

    added lodash get

--- index.js
+++ index.js
@@ -1,3 +1,10 @@
+// const lodash = require('lodash'); ----- 1 вариант
+const {get} = require('lodash'); //----- 2 вариант
+const a = {
+    name: 'Sergo'
+};
 
-const a = 3, b = 4;
-console.log('Hello', a + b);
+// const b = lodash.get(a, 'name','---'); ----- 1 вариант
+const b = get(a, 'name','---'); //----- 2 вариант
+
+console.log('Hello', b)
