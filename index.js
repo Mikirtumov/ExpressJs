0341614e9201d03d3fd19ce73ead10aac57dd01e
mikirtumov <mikirtumov@gmail.com>
2022-11-09 22:54:37 -0800

    eslint added

--- index.js
+++ index.js
@@ -2,12 +2,25 @@ const express = require('express');
 const app = express();
 const PORT = 3000;
 
-const home = (req, res) => {res.send('Hello World!')}
+// const home = (req, res) => {res.send('Hello World!')}
+// const demo = (req, res) => {res.send('Info here!!!')}
+
+function home(req, res){
+    res.send('Hello World!')
+}
+
+function demo(req, res){
+    const a = 123;
+    const b = Math.random();
+    const c = a + b;
+    res.send('Info HeRe!' + c);
+}
 
 
 
 
 app.get('/',home)
+app.post('/info',demo)
 
 app.listen(PORT, () => {
     console.log(`Example app listening ar port http://localhost:3000 ${PORT}`)
